fc697f99b8abbefeb22b4c814654c734bf5dbf58
Makasadbek aka Max <maksadbek@gmail.com>
2016-06-29 04:31:48 +0500

    python learning: data models

---
+++ python-learning/deck.py
@@ -0,0 +1,28 @@
+import collections
+from random import choice
+
+Card = collections.namedtuple('Card', ['rank', 'suit'])
+
+suit_values = dict(spades=3,hearts=2,diamonds=1,clubs=0)
+
+class FrenchDeck:
+    ranks = [str(n) for n in range(2,11)] + list('JQKA')
+    suits = 'spades diamonds clubs hearts'.split()
+
+    def __init__(self):
+        self._cards = [Card(rank,suit) for suit in self.suits for rank in self.ranks] 
+
+    def __len__(self):
+        return len(self._cards)
+
+    def __getitem__(self, position):
+        return self._cards[position]
+
+
+def spades_high(card):
+    rank_value = FrenchDeck.ranks.index(card.rank)
+    return rank_value * len(suit_values) + suit_values[card.suit]
+
+deck = FrenchDeck()
+
+print(choice(deck))
---
+++ python-learning/hello.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+def fuck():
+    print("fuck!!!")
+
+def sayHello(name):
+    print("hello ", name)
+
+
+sayHello("Max")
